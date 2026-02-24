019efb55231526e44cd52ade747ceb59260f7ea6
shivangnamu088-oss <shivangnamu088@gmail.com>
2026-02-24 10:22:25 +0530

    UC2: oops banner

--- oopsBannerApp.java
+++ oopsBannerApp.java
@@ -1,5 +1,13 @@
-public class oopsBannerApp{
-public static void main (String[] args){
-    System.out.println("OOPS");
-}
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****     *****    ******    *****  ");
+        System.out.println(" *     *   *     *   *     *  *     * ");
+        System.out.println("*       * *       *  *     *  *       ");
+        System.out.println("*       * *       *  ******    *****   ");
+        System.out.println("*       * *       *  *              *  ");
+        System.out.println(" *     *   *     *   *        *     *   ");
+        System.out.println("  *****     *****    *         *****    ");
+    }
 }
